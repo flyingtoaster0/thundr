20896ed0b6797622c56998b3ef5378faf76906d3
Tim Schonberger <timothy.schonberger@gmail.com>
2013-07-05 11:30:16 -0400

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LakeheadScheduler::Application.config.secret_token = 'ed097a841171439a093d9ce567f2331b83b121db1eaa5b218c6d4895967233b4b3f030e3b4a9bc5ad1e42c1999873a488071f1ccd6aa2c687b63f996d5422996'
