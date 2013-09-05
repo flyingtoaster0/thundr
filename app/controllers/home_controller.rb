5ddd943c5f106fe9a206b37001fac994cde6d258
Tim Schonberger <timothy.schonberger@gmail.com>
2013-09-04 22:51:57 -0400

    Homepage now displays a list of departments

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,5 +7,19 @@ class HomeController < ApplicationController
     #cookies.permanent[:test2] = ActiveSupport::JSON.encode(@testArr)
     #@arr = ActiveSupport::JSON.decode(cookies[:test2])
 
+
+    letters = ('A'..'F').to_a
+    _data = Department.where('substr(deptCode, 1, 1) IN (?)', letters)
+
+    @deptArray1 = _data.collect {|dept| {:deptCode => dept.deptCode, :deptName => dept.deptName }}
+
+
+    letters = ('G'..'M').to_a
+    _data = Department.where('substr(deptCode, 1, 1) IN (?)', letters)
+    @deptArray2 = _data.collect {|dept| {:deptCode => dept.deptCode, :deptName => dept.deptName }}
+
+    letters = ('N'..'Z').to_a
+    _data = Department.where('substr(deptCode, 1, 1) IN (?)', letters)
+    @deptArray3 = _data.collect {|dept| {:deptCode => dept.deptCode, :deptName => dept.deptName }}
   end
 end
