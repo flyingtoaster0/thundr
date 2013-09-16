d03f59ffbd86200431e5c441c79818a7b4e22289
flyingtoaster0 <Goombas5>
2013-09-16 19:24:26 -0400

    fixed heroku

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,17 +9,17 @@ class HomeController < ApplicationController
 
 
     letters = ('A'..'F').to_a
-    _data = Department.where('substr(deptCode, 1, 1) IN (?)', letters)
+    _data = Department.where('substr("deptCode", 1, 1) IN (?)', letters)
 
     @deptArray1 = _data.collect {|dept| {:deptCode => dept.deptCode, :deptName => dept.deptName }}
 
 
     letters = ('G'..'M').to_a
-    _data = Department.where('substr(deptCode, 1, 1) IN (?)', letters)
+    _data = Department.where('substr("deptCode", 1, 1) IN (?)', letters)
     @deptArray2 = _data.collect {|dept| {:deptCode => dept.deptCode, :deptName => dept.deptName }}
 
     letters = ('N'..'Z').to_a
-    _data = Department.where('substr(deptCode, 1, 1) IN (?)', letters)
+    _data = Department.where('substr("deptCode", 1, 1) IN (?)', letters)
     @deptArray3 = _data.collect {|dept| {:deptCode => dept.deptCode, :deptName => dept.deptName }}
   end
 end
