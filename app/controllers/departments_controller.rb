bd4779fc79743ddc8c32dadfc71525b2ff6714cb
flyingtoaster0 <Goombas5>
2013-09-18 20:36:56 -0400

    Department result page now lists courses

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -8,6 +8,7 @@ class DepartmentsController < ApplicationController
 
   def show
     @department = Department.find_by_deptCode(params[:id])
+    @courses = Course.find_all_by_department(params[:id])
   end
 
   def index
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ActiveRecord::Base
   has_and_belongs_to_many :schedules
   has_and_belongs_to_many :carts
-  attr_accessible :campus, :courseCode, :description, :endDate, :endTime, :fri, :instructor, :mon, :name, :notes, :room, :sat, :startDate, :startTime, :sun, :thu, :tue, :wed
+  attr_accessible :campus, :name, :courseCode, :description, :endDate, :endTime, :fri, :instructor, :mon, :name, :notes, :room, :sat, :startDate, :startTime, :sun, :thu, :tue, :wed
 end
