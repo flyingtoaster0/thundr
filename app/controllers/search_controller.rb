b5f31b4d9a239763f8a8ca3d417f216991c44a89
flyingtoaster0 <Goombas5>
2013-09-27 00:30:42 -0400

    More ui improvements. Fixed searching.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,19 +1,20 @@
 class SearchController < ApplicationController
   def index
     course_info = params[:q].split('-')
-    dept_code = course_info[0]
+    @query = course_info[0] ? course_info[0] : ''
+    dept_code = @query.upcase
     course_code = course_info[1]
 
     if course_code
-      course = Course.find_by_department_and_code(dept_code, course_code)
+      course = Course.find_by_department_and_code(@dept_code, course_code)
 
       if course
-        redirect_to('/courses/'+dept_code+'/'+course_code)
+        redirect_to('/courses/'+@dept_code+'/'+course_code)
       end
     else
-      department = Department.find_by_deptCode(dept_code)
+      department = Department.find_by_deptCode(@dept_code)
       if department
-        redirect_to('/departments/'+dept_code)
+        redirect_to('/departments/'+@dept_code)
       end
     end
   end
