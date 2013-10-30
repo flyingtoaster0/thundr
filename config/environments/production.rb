350c1fd5aa6ae0e8da1e0970e084141ac7acaac1
flyingtoaster0 <flyingtoaster0@gmail.com>
2013-10-30 14:11:41 -0400

    Fixed heroku bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,9 +45,11 @@ LakeheadScheduler::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
+
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-
+  config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
