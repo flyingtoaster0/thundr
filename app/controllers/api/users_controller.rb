ecccea42906c385b56da9c30c93c3acfcb8bfe7e
flyingtoaster0 <Goombas5>
2013-12-06 11:25:42 -0500

    Login via HTTP requests works. Returns a remember token.

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,19 @@
+class API::UsersController < ApplicationController
+  def index
+
+  end
+
+  #Gets the username name password from the HTTP request, authenticates, and returns a remember token if successful
+  def login
+    #localhost:3000/api/login -u 'admin:password'
+    authenticate_or_request_with_http_basic do |username, password|
+      user = User.find_by_username(username.downcase)
+      if user && user.authenticate(password)
+        remember_token = RememberToken.create(token: User.encrypt(User.new_remember_token), user_id: user.id, accessed_at: Time.now)
+        respond_to do |format|
+          format.json { render :json => remember_token }
+        end
+      end
+    end
+  end
+end
